cab377a55a94d9dbf1ef301e9ef0f5b4f8f288e4
hestu14 <hestu.14@gmail.com>
2020-09-01 22:04:49 +0700

    Updates

---
+++ precache-manifest.76acb5b3381e67d8e772a093deeb2820.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c94bb9aff101aa84c9bc2f911ba1bdf",
+    "url": "/mini_currency/index.html"
+  },
+  {
+    "revision": "13bc42a1f5fc8d5f2a04",
+    "url": "/mini_currency/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "03f5a1ab665f6035011c",
+    "url": "/mini_currency/static/css/main.ed9f4ba0.chunk.css"
+  },
+  {
+    "revision": "13bc42a1f5fc8d5f2a04",
+    "url": "/mini_currency/static/js/2.31f2a744.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mini_currency/static/js/2.31f2a744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f5a1ab665f6035011c",
+    "url": "/mini_currency/static/js/main.29d03e42.chunk.js"
+  },
+  {
+    "revision": "243a0d48b3208402af52",
+    "url": "/mini_currency/static/js/runtime-main.39483165.js"
+  }
+]);
